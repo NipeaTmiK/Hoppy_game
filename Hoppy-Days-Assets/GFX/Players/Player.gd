0d18de11926d5f81bcc8edcd8967980ed3ae0a9e
NipeaTmiK <iamkokkok@hotmail.com>
2019-09-19 10:27:48 +0700

    Add first version

---
+++ Hoppy-Days-Assets/GFX/Players/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,100)
+
+export var SPEED = 500
+export var Gravity = 500
+	
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	
+	else:
+		motion.x = 0
+	
+	if Input.is_action_pressed("jump"):
+		motion.y = -Gravity
+	else:
+		motion.y = Gravity
+	
+	
+	move_and_slide(motion)
+	
+
+	
+	
