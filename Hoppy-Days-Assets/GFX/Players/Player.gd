6bbfa0b604a768fe12e6262cc5872491d44987ee
NipeaTmiK <iamkokkok@hotmail.com>
2019-09-19 11:33:20 +0700

    Update Ver. 1

--- Hoppy-Days-Assets/GFX/Players/Player.gd
+++ Hoppy-Days-Assets/GFX/Players/Player.gd
@@ -1,28 +1,33 @@
 extends KinematicBody2D
 
-var motion = Vector2(0,100)
+var motion = Vector2(0,0)
 
 export var SPEED = 500
-export var Gravity = 500
+export var Gravity = 10
+export var jump_power = 500
+const UP = Vector2 (0,-1)
 	
 func _physics_process(delta):
 	
+	apply_gravity()
+	jump()
+	walk()
+	
+	move_and_slide(motion,UP)
+	
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	
+	motion.y += Gravity
+	
+func jump():
+	if Input.is_action_just_pressed("jump"):
+		motion.y = -jump_power
+func walk():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
-	
 	else:
-		motion.x = 0
-	
-	if Input.is_action_pressed("jump"):
-		motion.y = -Gravity
-	else:
-		motion.y = Gravity
-	
-	
-	move_and_slide(motion)
-	
-
-	
-	
+		motion.x= 0
